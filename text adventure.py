e77e3ff5de0aeb291602b155eb9265a06383b2b4
bramuige <bramuige@gmail.Com>
2016-08-08 22:17:06 +0200

    nog niet helemaal af

--- text adventure.py
+++ text adventure.py
@@ -1,10 +1,13 @@
+from random import randrange
+import random
+hprat = randrange(10)
 print("welkom in het avontuur")
 print("het wijst zichzelf wel uit")
 print("daar is het eerste monster")
-print("ik ben een rat level 2")
-hprat = "2"
+print("ik ben een rat level " + hprat)
+print(hprat)
 print("welke aanval wil je doen")
+print("de rat heeft " + hprat + " hp")
 print("kak gooien (+3)")
 print("zeiken op dat ding jonge(-3)")
 aanval = raw_input("wat kies je? ")
-print(aanval + " mooie keuze")
